b2e61f4136ef33ecb2791d5633e974f65b80beb9
Ilya <iluhapolitov@gmail.com>
2023-05-27 23:00:42 +0300

    Made third one

---
+++ Task3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [] array = new int[n];
+Console.Write("Таблица кубов: ");
+for(int i = 0;i < array.Length;i++){
+    array[i] = (i + 1)*(i + 1)*(i + 1);
+    Console.Write(array[i] + " ");
+}
